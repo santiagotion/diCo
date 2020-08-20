0be900a468b3cbfb9d75c36a79b0ef1cf084c3fe
Nathan Tuala <samsonytuala@gmail.com>
2020-08-19 19:59:24 -0500

    I changed something

--- diCo/LibraryVC.swift
+++ diCo/LibraryVC.swift
@@ -28,7 +28,7 @@ class LibraryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "toDetails1", sender: indexPath)
         
-        print("Hello Santi, this is Nathan from heaven.")
+        print("Hello Santi, this is Nathan from heaven. Because I don't care ")
         
     }
     
